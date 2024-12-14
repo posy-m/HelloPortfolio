11f59eadaea9f0a3f307531e0271a1b448ff95f6
posy-m <minji.posy@gmail.com>
2024-12-15 01:07:52 +0900

    modal 레이아웃

--- src/project/Project.jsx
+++ src/project/Project.jsx
@@ -9,17 +9,55 @@ import inside from '../images/wine.png'
 import donutshop from '../images/donutshop.png'
 import Modal from './modal/Modal';
 
+//img
+import uphill0 from '../images/unmountin.gif'
+import uphill1 from '../projectImg/uphill1.jpg'
+import uphill2 from '../projectImg/uphill2.jpg'
+import uphill3 from '../projectImg/uphill3.jpg'
+import uphill4 from '../projectImg/uphill4.jpg'
+import uphill5 from '../projectImg/uphill5.jpg'
+import uphill6 from '../projectImg/uphill6.jpg'
+import uphill7 from '../projectImg/uphill7.jpg'
+import uphill8 from '../projectImg/uphill8.jpg'
+import uphill9 from '../projectImg/uphill9.jpg'
+import insidout1 from '../projectImg/insideout1.png'
+import insidout2 from '../projectImg/insideout2.png'
+import insidout3 from '../projectImg/insideout3.gif'
+import insidout4 from '../projectImg/insideout4.gif'
+import donut0 from '../images/donut.gif'
+import donut1 from '../projectImg/donut1.png'
+import donut2 from '../projectImg/donut2.png'
+import donut3 from '../projectImg/donut3.png'
+import donut4 from '../projectImg/donut4.png'
+import donut5 from '../projectImg/donut5.png'
+
+
+
 const apptechImages = [
-  { src: apptech, alt: 'AppTech Image 1' },
-  { src: apptech, alt: 'AppTech Image 2' },
+  { src: uphill0, alt: 'AppTech Image 1' },
+  { src: uphill1, alt: 'AppTech Image 2' },
+  { src: uphill2, alt: 'AppTech Image 3' },
+  { src: uphill3, alt: 'AppTech Image 4' },
+  { src: uphill4, alt: 'AppTech Image 5' },
+  { src: uphill5, alt: 'AppTech Image 6' },
+  { src: uphill6, alt: 'AppTech Image 7' },
+  { src: uphill7, alt: 'AppTech Image 8' },
+  { src: uphill8, alt: 'AppTech Image 9' },
+  { src: uphill9, alt: 'AppTech Image 10' },
 ];
 const insideImages = [
-  { src: inside, alt: 'Wine Community Image 1' },
-  { src: inside, alt: 'Wine Community Image 2' },
+  { src: insidout3, alt: 'Wine Community Image 3' },
+  { src: insidout4, alt: 'Wine Community Image 4' },
+  { src: insidout1, alt: 'Wine Community Image 1' },
+  { src: insidout2, alt: 'Wine Community Image 2' },
 ];
 const donutshopImages = [
-  { src: donutshop, alt: 'Donut Shop Image 1' },
-  { src: donutshop, alt: 'Donut Shop Image 2' },
+  { src: donut0, alt: 'Donut Shop Image 1' },
+  { src: donut1, alt: 'Donut Shop Image 2' },
+  { src: donut2, alt: 'Donut Shop Image 3' },
+  { src: donut3, alt: 'Donut Shop Image 4' },
+  { src: donut4, alt: 'Donut Shop Image 5' },
+  { src: donut5, alt: 'Donut Shop Image 6' },
 ];
 
 const Project = () => {
--- src/project/modal/Modal.jsx
+++ src/project/modal/Modal.jsx
@@ -3,13 +3,17 @@ import styles from './modal.module.css'
 import x from '../../images/x.png'
 import test from '../../images/unmountin.gif'
 import notion from '../../images/notion.png'
+import github from '../../images/github.png'
+import link from '../../images/link.png'
 
 import Slider from 'react-slick';
 
 
-const Modal = ({ isOpen, onClose, content }) => {
+const Modal = ({ isOpen, onClose, content, images }) => {
   if (!isOpen) return null;
 
+
+  // 다하고 지우기
   const imageArray = [
     { id: 1, src: test, alt: 'Image 1' },
     { id: 2, src: test, alt: 'Image 2' },
@@ -36,9 +40,6 @@ const Modal = ({ isOpen, onClose, content }) => {
         <div className={styles.modalLine}></div>
         {content && (
           <div className={styles.contentBox}>
-            {/* <div className={styles.mainimg}>
-              <img src={test} alt="" />
-            </div> */}
             <div className={styles.slider}>
               <Slider {...settings}>
                 {imageArray.map((image) => (
@@ -47,20 +48,58 @@ const Modal = ({ isOpen, onClose, content }) => {
                   </div>
                 ))}
               </Slider>
+              {/* <Slider {...settings}>
+                  {images.map((image, index) => (
+                    <div key={index} className={styles.mainimg}>
+                      <img src={image.src} alt={image.alt} className={styles.slideImage} />
+                    </div>
+                  ))}
+                </Slider> */}
+
             </div>
             <div className={styles.linkBox}>
-              <p><link rel="stylesheet" href="" target="_blank" />  <img src={notion} alt="" /></p>
-              <p><link rel="stylesheet" href="" target="_blank" />  <img src={notion} alt="" /></p>
-              <p><link rel="stylesheet" href="" target="_blank" />  <img src={notion} alt="" /></p>
+              <a rel="noopener noreferrer" href="" target="_blank">
+                <img src={github} alt="GitHub" />
+              </a>
+              <a rel="noopener noreferrer" href="" target="_blank">
+                <img src={notion} alt="Notion" />
+              </a>
+              {/* {images.linkNameHomepage && (
+                <a rel="noopener noreferrer" href={images.linkNameHomepage} target="_blank">
+                  <img src={link} alt="Link" />
+                </a>
+              )} */}
             </div>
-            <div>
-              <span>날짜</span>
-              <span>설명</span>
+            <div className={styles.info}>
+              <div className={styles.infoData}>
+                <span>2024.09.24-2024.10.24(30일)</span>
+                <span>5명(Frontend:3명, Backend:2명)</span>
+              </div>
+              <div className={styles.stack}>
+                <span>nextjs</span>
+                <span>nextjs</span>
+                <span>nextjs</span>
+                <span>nextjs</span>
+              </div>
             </div>
+            <div className={styles.introduction}>
+              <span>
+                <p>산을 오르며 고도에 따라 포인트를 적립하고,
+                  적립 된 포인트로 다양한 상품이나 캐릭터를 구매하여 즐길 수 있는 어플입니다.</p>
+              </span>
+            </div>
+            <div className={styles.charge}>
+              <span>맡은기능</span>
+              <ul>
+                <li>사용자가 입력하는 데이터를 유효성 검사를 거쳐 회원가입 기능 구현</li>
+                <li>사용자가 입력하는 데이터를 유효성 검사를 거쳐 회원가입 기능 구현</li>
 
+              </ul>
 
+            </div>
           </div >
-        )}
+        )
+        }
 
       </div >
     </div >
