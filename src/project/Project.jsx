6858fd84945e89c8a3887cfa88ae85c04440cf84
posy-m <minji.posy@gmail.com>
2024-12-19 21:07:35 +0900

    img modal

--- src/project/Project.jsx
+++ src/project/Project.jsx
@@ -31,7 +31,7 @@ import donut3 from '../projectImg/donut3.png'
 import donut4 from '../projectImg/donut4.png'
 import donut5 from '../projectImg/donut5.png'
 
-
+import Slider from 'react-slick';
 
 const apptechImages = [
   { src: uphill0, alt: 'AppTech Image 1' },
@@ -45,6 +45,8 @@ const apptechImages = [
   { src: uphill8, alt: 'AppTech Image 9' },
   { src: uphill9, alt: 'AppTech Image 10' },
 ];
+
+
 const insideImages = [
   { src: insidout3, alt: 'Wine Community Image 3' },
   { src: insidout4, alt: 'Wine Community Image 4' },
@@ -63,9 +65,12 @@ const donutshopImages = [
 const Project = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalContent, setModalContent] = useState(null);
+  const [modalImages, setModalImages] = useState(null);
 
-  const openMoal = (content) => {
+  const openMoal = (content, images) => {
+    console.log("Modal Content: ", content); // 전달되는 데이터 확인
     setModalContent(content);
+    setModalImages(images);
     setIsModalOpen(true);
   }
 
@@ -96,8 +101,16 @@ const Project = () => {
             content='산을 오르며 고도에 따라 포인트를 적립할 수 있도록 하이브리드 앱을 구현 했습니다.'
             hashtag='Hybrid App'
             onClick={() => openMoal({
-
-            })} />
+              title: "등산 앱테크",
+              introduction: "산을 오르며 고도에 따라 포인트를 적립하고,적립 된 포인트로 다양한 상품이나 캐릭터를 구매하여 즐길 수 있는 어플입니다.",
+              chargeConent: [
+                "사용자가 입력하는 데이터를 유효성 검사를 거쳐 회원가입 기능 구현",
+                "Jotai를 활용해 회원 정보를 전역 상태에서 불러와 검증한 후, API 요청을 통해 로그인 기능 구현",
+                "Firebase에서 제공하는 Firebase Authentication 서비스를 사용하여 휴대폰 번호로 비밀번호 찾기 / 아이디 찾기 구현",
+                "포인트로 구매한 상품을 ‘사용 중’과 ‘사용 완료’ 상태로 분류하여 보관할 수 있는 기능을 구현",
+                "React-Query에서 제공하는 useInfiniteQuery 훅을 사용하여 무한스크롤 구현",
+              ],
+            }, apptechImages)} />
           <Square
             src={inside}
             title='와인커뮤니티 사이트'
@@ -109,10 +122,11 @@ const Project = () => {
             content='데이터 저장소 기능 대신 사용할 수 있는 로컬 스토리지와 세션스토리지를 사용하여 커뮤니티 사이트를 구현했습니다.'
             hashtag='Web' />
         </div>
-        <Modal isOpen={isModalOpen} onClose={closeModal} content={modalContent} />
+        <Modal isOpen={isModalOpen} onClose={closeModal} content={modalContent} images={modalImages} />
       </div>
     </Element>
   )
 }
 
 export default Project
+
--- src/project/modal/Modal.jsx
+++ src/project/modal/Modal.jsx
@@ -9,16 +9,16 @@ import link from '../../images/link.png'
 import Slider from 'react-slick';
 
 
-const Modal = ({ isOpen, onClose, content, images }) => {
+const Modal = ({ isOpen, onClose, content, images = [] }) => {
   if (!isOpen) return null;
 
 
   // 다하고 지우기
-  const imageArray = [
-    { id: 1, src: test, alt: 'Image 1' },
-    { id: 2, src: test, alt: 'Image 2' },
-    { id: 3, src: notion, alt: 'Image 3' },
-  ];
+  // const imageArray = [
+  //   { id: 1, src: test, alt: 'Image 1' },
+  //   { id: 2, src: test, alt: 'Image 2' },
+  //   { id: 3, src: notion, alt: 'Image 3' },
+  // ];
 
   const settings = {
     dots: true, // 하단 네비게이션 점 표시
@@ -28,11 +28,23 @@ const Modal = ({ isOpen, onClose, content, images }) => {
     slidesToScroll: 1, // 스크롤할 슬라이드 수
   };
 
+  // const slider = (
+  //   <Slider {...settings}>
+  //     {images.map((image, index) => (
+  //       <div key={index} className={styles.mainimg}>
+  //         <img src={image.src} alt={image.alt} className={styles.slideImage} />
+  //       </div>
+  //     ))}
+  //   </Slider>
+  // );
+
+
+
   return (
     <div className={styles.modalOverlay} onClick={onClose}>
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
         <header className={styles.header}>
-          <sapn className={styles.title}>제목</sapn>
+          <sapn className={styles.title}>{content.title}</sapn>
           <button className={styles.closeButton} onClick={onClose}>
             <img src={x} alt="닫기" />
           </button>
@@ -41,21 +53,27 @@ const Modal = ({ isOpen, onClose, content, images }) => {
         {content && (
           <div className={styles.contentBox}>
             <div className={styles.slider}>
-              <Slider {...settings}>
+              {/* <Slider {...settings}>
                 {imageArray.map((image) => (
                   <div key={image.id} className={styles.mainimg}>
                     <img src={image.src} alt={image.alt} className={styles.slideImage} />
                   </div>
                 ))}
-              </Slider>
+              </Slider> */}
               {/* <Slider {...settings}>
-                  {images.map((image, index) => (
-                    <div key={index} className={styles.mainimg}>
-                      <img src={image.src} alt={image.alt} className={styles.slideImage} />
-                    </div>
-                  ))}
-                </Slider> */}
-
+                {images.map((image, index) => (
+                  <div key={index} className={styles.mainimg}>
+                    <img src={content.src} alt={image.alt} className={styles.slideImage} />
+                  </div>
+                ))}
+              </Slider> */}
+              <Slider {...settings}>
+                {images.map((image, index) => (
+                  <div key={index} className={styles.mainimg}>
+                    <img src={image.src} alt={image.alt} className={styles.slideImage} />
+                  </div>
+                ))}
+              </Slider>
             </div>
             <div className={styles.linkBox}>
               <a rel="noopener noreferrer" href="" target="_blank">
@@ -64,11 +82,11 @@ const Modal = ({ isOpen, onClose, content, images }) => {
               <a rel="noopener noreferrer" href="" target="_blank">
                 <img src={notion} alt="Notion" />
               </a>
-              {/* {images.linkNameHomepage && (
+              {images.linkNameHomepage && (
                 <a rel="noopener noreferrer" href={images.linkNameHomepage} target="_blank">
                   <img src={link} alt="Link" />
                 </a>
-              )} */}
+              )}
             </div>
             <div className={styles.info}>
               <div className={styles.infoData}>
@@ -84,18 +102,32 @@ const Modal = ({ isOpen, onClose, content, images }) => {
             </div>
             <div className={styles.introduction}>
               <span>
-                <p>산을 오르며 고도에 따라 포인트를 적립하고,
-                  적립 된 포인트로 다양한 상품이나 캐릭터를 구매하여 즐길 수 있는 어플입니다.</p>
+                {/* <p>산을 오르며 고도에 따라 포인트를 적립하고,
+                  적립 된 포인트로 다양한 상품이나 캐릭터를 구매하여 즐길 수 있는 어플입니다.</p> */}
+                <p>{content.introduction}</p>
               </span>
             </div>
             <div className={styles.charge}>
               <span>맡은기능</span>
               <ul>
-                <li>사용자가 입력하는 데이터를 유효성 검사를 거쳐 회원가입 기능 구현</li>
-                <li>사용자가 입력하는 데이터를 유효성 검사를 거쳐 회원가입 기능 구현</li>
-
+                {content.chargeConent.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
               </ul>
-
+            </div>
+            <div className={styles.issue}>
+              <span>이슈사항</span>
+              <div className={styles.issueWrap}>
+                <div className={styles.problem}>
+                  <span>문제점</span>
+                  <p>상품 및 아바타 컴포넌트를 로드 할 때 메모리를 절약하기 위해 한 번에 전체 데이터를 불러오는 것보다 한 페이지 당 데이터의 개수 지정하여 불러오려고 사용하였으나
+                    무한 스크롤을 구현하는 과정에서 초기에는 첫 페이지가 정상적으로 렌더링이 되었으나 두 번째 페이지 이후부터 리렌더링이 되지 않는 문제 발생</p>
+                </div>
+                <div className={styles.problem}>
+                  <span>해결방안</span>
+                  <p>다음 페이지 여부와 로드 할 데이터가 있는지 서버 상태를 주시하고, 매번 새로운 페이지로 넘어 갈 때마다 정상적으로 컴포넌트가 리렌더링 될 수 있도록 설정하여  메모리를 보다 효율적으로 사용할 수 있게 했고 데이터를 분할하여 로드 할 수 있게 함</p>
+                </div>
+              </div>
             </div>
           </div >
         )
@@ -107,3 +139,5 @@ const Modal = ({ isOpen, onClose, content, images }) => {
 }
 
 export default Modal
+
+
