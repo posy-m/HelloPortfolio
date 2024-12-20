866ec975c5030fb00d2b42b7ed67f95acbc78a4d
posy-m <minji.posy@gmail.com>
2024-12-20 18:14:28 +0900

    project modal

--- src/project/Project.jsx
+++ src/project/Project.jsx
@@ -31,7 +31,6 @@ import donut3 from '../projectImg/donut3.png'
 import donut4 from '../projectImg/donut4.png'
 import donut5 from '../projectImg/donut5.png'
 
-import Slider from 'react-slick';
 
 const apptechImages = [
   { src: uphill0, alt: 'AppTech Image 1' },
@@ -68,7 +67,7 @@ const Project = () => {
   const [modalImages, setModalImages] = useState(null);
 
   const openMoal = (content, images) => {
-    console.log("Modal Content: ", content); // 전달되는 데이터 확인
+    // console.log("Modal Content: ", content);
     setModalContent(content);
     setModalImages(images);
     setIsModalOpen(true);
@@ -102,6 +101,11 @@ const Project = () => {
             hashtag='Hybrid App'
             onClick={() => openMoal({
               title: "등산 앱테크",
+              date: "2024.09.24-2024.10.24(30일)",
+              personnel: "5명(Frontend:3명, Backend:2명)",
+              linkName: "https://github.com/posy-m/UPHiLLProjectFront",
+              linkNameNotion: "https://lavish-spike-71b.notion.site/UPHiLL-10bf53737a1380aa8acff5244d02823b",
+              linkNameHomepage: "https://uphillmountain.store",
               introduction: "산을 오르며 고도에 따라 포인트를 적립하고,적립 된 포인트로 다양한 상품이나 캐릭터를 구매하여 즐길 수 있는 어플입니다.",
               chargeConent: [
                 "사용자가 입력하는 데이터를 유효성 검사를 거쳐 회원가입 기능 구현",
@@ -110,17 +114,56 @@ const Project = () => {
                 "포인트로 구매한 상품을 ‘사용 중’과 ‘사용 완료’ 상태로 분류하여 보관할 수 있는 기능을 구현",
                 "React-Query에서 제공하는 useInfiniteQuery 훅을 사용하여 무한스크롤 구현",
               ],
+              skill: ['JavaScript', 'TypeScript', 'React', 'Next.js'],
+              problem: ["상품 및 아바타 컴포넌트를 로드 할 때 메모리를 절약하기 위해 한 번에 전체 데이터를 불러오는 것보다 한 페이지 당 데이터의 개수 지정하여 불러오려고 사용하였으나 무한 스크롤을 구현하는 과정에서 초기에는 첫 페이지가 정상적으로 렌더링이 되었으나 두 번째 페이지 이후부터 리렌더링이 되지 않는 문제 발생"],
+              solution: ["다음 페이지 여부와 로드 할 데이터가 있는지 서버 상태를 주시하고, 매번 새로운 페이지로 넘어 갈 때마다 정상적으로 컴포넌트가 리렌더링 될 수 있도록 설정하여  메모리를 보다 효율적으로 사용할 수 있게 했고 데이터를 분할하여 로드 할 수 있게 함"]
             }, apptechImages)} />
           <Square
             src={inside}
-            title='와인커뮤니티 사이트'
+            title='주류 커뮤니티 사이트'
             content='프론트엔드와 백엔드 간의 데이터 송수신 원리를 활용하여 커뮤니티 사이트를 구현했습니다.'
-            hashtag='Web' />
+            hashtag='Web'
+            onClick={() => openMoal({
+              title: "주류 커뮤니티 사이트",
+              date: "2024.09.24-2024.10.24(30일)",
+              linkName: "https://github.com/posy-m/InsideOut",
+              linkNameNotion: "https://lavish-spike-71b.notion.site/InsideOut-52b02e18ce2c4ec0ae5ba5a5848185ad",
+              personnel: "4명 (Frontend, Backend)",
+              introduction: "산을 오르며 고도에 따라 포인트를 적립하고,적립 된 포인트로 다양한 상품이나 캐릭터를 구매하여 즐길 수 있는 어플입니다.",
+              chargeConent: [
+                " 로그인한 회원이 제목과 내용 작성과 Multer를 이용해 이미지를 업로드 할 수 있는 게시글 작성 기능을 구현",
+                "게시글의 고유한 ID값을 요청 받아 데이터베이스에서 해당 데이터를 조회하고 Sequelize를 사용하여 서버에서 수정 및 삭제 기능을 구현",
+                " 한 게시글에 대한 댓글과 대댓글을 작성할 수 있도록 게시글과 댓글간에 1:N관계를 설정하여 댓글과 대댓글은 회원 테이블의 고유 ID와 외래키를 작성자와 닉네임을 매칭하며, 각 댓글과 대댓글은 게시글에 속할 수 있도록 글과 대댓글간에 관계를 설정하여 기능 구현",
+                "사용자가 입력한 제목과 게시글 내용 검색어를 서버로 쿼리 스트링을 전송하여 검색 기능 구현 ",
+                "현재 페이지의 URL경로를 확인하여 쿼리 문자열을 제외한 경로만 추출하여 경로에 따라 다른 카테고리 값을 할당하여 특정 카테고리 번호를 결정하는 로직을 구현",
+                "한 페이지에 표시할 데이터 수와 현재 페이지 값을 쿼리스트링으로 서버에 전달받아 데이터를 데이터베이스에서 조회하여 반환하여 페이지네이션 기능 구현",
+              ],
+              skill: ['HTML5', 'CSS3', 'JavaScript', 'TypeScript', 'Nestjs', 'mysql', 'Sequelize'],
+              problem: ["클라이언트가 데이터 수정 또는 삭제페이지에서 다음 페이지로 이동이 되지 않는 문제 발생", "댓글과 대댓글 작성 시 데이터가 복잡하게 얽혀 있어, 잘못된 외래키 값이 저장되어 데이터베이스 무결성을 해치는 문제가 발생."],
+              solution: ["클라이언트에서 HTTP 상태 코드와 함께 상세한 에러 메시지를 반환하도록 예외 처리를 추가.", "데이터베이스에서 댓글과 대댓글 간의 관계를 정확히 설정하고, 작성자 ID와 게시글 ID를 검증하여 요청된 게시글 ID를 기준으로 필요한 댓글만 서버에서 조회하는 로직을 구현하여 해결"]
+            }, insideImages)} />
           <Square
             src={donutshop}
             title='커뮤니티 사이트'
             content='데이터 저장소 기능 대신 사용할 수 있는 로컬 스토리지와 세션스토리지를 사용하여 커뮤니티 사이트를 구현했습니다.'
-            hashtag='Web' />
+            hashtag='Web'
+            onClick={() => openMoal({
+              title: "도넛 커뮤니티 사이트",
+              date: "2024.05.14-2024.05.28 (14일)",
+              linkName: "https://github.com/posy-m/songpa",
+              linkNameNotion: "https://mirror-check-8ea.notion.site/TEAM-a58a93c037034dc9b6d6bbf11cdbc063",
+              personnel: "4명 (Frontend)",
+              introduction: "데이터베이스 없이 데이터 저장소 기능 대신 사용할 수있는 로컬 스토리지와 타입스크립트를 사용하여 홈페이지에 유입된 고객이 게시판을 통해 자유롭게 소통할 수 있는 커뮤니케이션 사이트 입니다.",
+              chargeConent: [
+                "TypeScript의 정적 타입검사를 활용하여 게시판 데이터 구조를 미리 정의하여 타입 오류를 방지하고 JavaScript로 빌드하여 실제 웹 환경에서 동작하도록 구현",
+                "로컬스토리지에 저장된 게시판 데이터를 정의한 타입에 맞게 값을 받아와 나누어 보여주기 위해 페이지 네이션 로직 구현",
+                "사용자가 입력한 검색어를 쿼리 파라미터로 전달하여 해당 검색어가 포함된 제목을 필터링을 할때 타입스크립트의 타입검사를 활용 하여 검색 기능 구현"
+              ],
+              skill: ['HTML5', 'CSS3', 'JavaScript', 'TypeScript'],
+              problem: ["TypeScript로 게시판 데이터 구조를 정의했으나, 예상치 못한 동적 데이터가 들어올 경우 타입 오류가 발생하거나, 타입이 맞지 않아 실행 중 오류가 발생."],
+              solution: ["게시판 데이터 구조를 interface와 type을 사용해 명확히 정의하여 동적 데이터를 처리할 수 있도록 설계."]
+            }, donutshopImages)}
+          />
         </div>
         <Modal isOpen={isModalOpen} onClose={closeModal} content={modalContent} images={modalImages} />
       </div>
--- src/project/modal/Modal.jsx
+++ src/project/modal/Modal.jsx
@@ -13,7 +13,6 @@ const Modal = ({ isOpen, onClose, content, images = [] }) => {
   if (!isOpen) return null;
 
 
-  // 다하고 지우기
   // const imageArray = [
   //   { id: 1, src: test, alt: 'Image 1' },
   //   { id: 2, src: test, alt: 'Image 2' },
@@ -26,18 +25,8 @@ const Modal = ({ isOpen, onClose, content, images = [] }) => {
     speed: 500, // 전환 속도(ms)
     slidesToShow: 1, // 한 번에 보여줄 슬라이드 수
     slidesToScroll: 1, // 스크롤할 슬라이드 수
+    fade: true
   };
-
-  // const slider = (
-  //   <Slider {...settings}>
-  //     {images.map((image, index) => (
-  //       <div key={index} className={styles.mainimg}>
-  //         <img src={image.src} alt={image.alt} className={styles.slideImage} />
-  //       </div>
-  //     ))}
-  //   </Slider>
-  // );
-
 
 
   return (
@@ -53,20 +42,6 @@ const Modal = ({ isOpen, onClose, content, images = [] }) => {
         {content && (
           <div className={styles.contentBox}>
             <div className={styles.slider}>
-              {/* <Slider {...settings}>
-                {imageArray.map((image) => (
-                  <div key={image.id} className={styles.mainimg}>
-                    <img src={image.src} alt={image.alt} className={styles.slideImage} />
-                  </div>
-                ))}
-              </Slider> */}
-              {/* <Slider {...settings}>
-                {images.map((image, index) => (
-                  <div key={index} className={styles.mainimg}>
-                    <img src={content.src} alt={image.alt} className={styles.slideImage} />
-                  </div>
-                ))}
-              </Slider> */}
               <Slider {...settings}>
                 {images.map((image, index) => (
                   <div key={index} className={styles.mainimg}>
@@ -76,34 +51,31 @@ const Modal = ({ isOpen, onClose, content, images = [] }) => {
               </Slider>
             </div>
             <div className={styles.linkBox}>
-              <a rel="noopener noreferrer" href="" target="_blank">
+              <a rel="noopener noreferrer" href={content.linkName} target="_blank">
                 <img src={github} alt="GitHub" />
               </a>
-              <a rel="noopener noreferrer" href="" target="_blank">
+              <a rel="noopener noreferrer" href={content.linkNameNotion} target="_blank">
                 <img src={notion} alt="Notion" />
               </a>
-              {images.linkNameHomepage && (
-                <a rel="noopener noreferrer" href={images.linkNameHomepage} target="_blank">
+              {content.linkNameHomepage && (
+                <a rel="noopener noreferrer" href={content.linkNameHomepage} target="_blank">
                   <img src={link} alt="Link" />
                 </a>
               )}
             </div>
             <div className={styles.info}>
               <div className={styles.infoData}>
-                <span>2024.09.24-2024.10.24(30일)</span>
-                <span>5명(Frontend:3명, Backend:2명)</span>
+                <span>{content.date}</span>
+                <span>{content.personnel}</span>
               </div>
               <div className={styles.stack}>
-                <span>nextjs</span>
-                <span>nextjs</span>
-                <span>nextjs</span>
-                <span>nextjs</span>
+                {content.skill.map((skill, index) => (
+                  <span key={index}>{skill}</span>
+                ))}
               </div>
             </div>
             <div className={styles.introduction}>
               <span>
-                {/* <p>산을 오르며 고도에 따라 포인트를 적립하고,
-                  적립 된 포인트로 다양한 상품이나 캐릭터를 구매하여 즐길 수 있는 어플입니다.</p> */}
                 <p>{content.introduction}</p>
               </span>
             </div>
@@ -120,12 +92,21 @@ const Modal = ({ isOpen, onClose, content, images = [] }) => {
               <div className={styles.issueWrap}>
                 <div className={styles.problem}>
                   <span>문제점</span>
-                  <p>상품 및 아바타 컴포넌트를 로드 할 때 메모리를 절약하기 위해 한 번에 전체 데이터를 불러오는 것보다 한 페이지 당 데이터의 개수 지정하여 불러오려고 사용하였으나
-                    무한 스크롤을 구현하는 과정에서 초기에는 첫 페이지가 정상적으로 렌더링이 되었으나 두 번째 페이지 이후부터 리렌더링이 되지 않는 문제 발생</p>
+                  {/* <div className={styles.modalLine}></div> */}
+                  <ul>
+                    {content.problem.map((item, index) => (
+                      <li key={index}>{item}</li>
+                    ))}
+                  </ul>
                 </div>
                 <div className={styles.problem}>
                   <span>해결방안</span>
-                  <p>다음 페이지 여부와 로드 할 데이터가 있는지 서버 상태를 주시하고, 매번 새로운 페이지로 넘어 갈 때마다 정상적으로 컴포넌트가 리렌더링 될 수 있도록 설정하여  메모리를 보다 효율적으로 사용할 수 있게 했고 데이터를 분할하여 로드 할 수 있게 함</p>
+                  {/* <div className={styles.modalLine}></div> */}
+                  <ul>
+                    {content.solution.map((item, index) => (
+                      <li key={index}>{item}</li>
+                    ))}
+                  </ul>
                 </div>
               </div>
             </div>
