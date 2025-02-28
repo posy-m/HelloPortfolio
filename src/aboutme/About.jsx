4d3ba78e14bf4d13780964604f308e47e5a7967c
posy-m <minji.posy@gmail.com>
2025-02-28 17:43:09 +0900

    수정

--- src/aboutme/About.jsx
+++ src/aboutme/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
     <div className={styles.aboutBox}>
       <div className={styles.aboutWrap}>
         <section className={styles.aboutTextBox}>
-          <h1>프론트엔드 개발자 김민지 입니다.</h1>
+          <h1>Developer 김민지 입니다.</h1>
           <div className={styles.aboutSubText}>
             <p>문제 해결을 염두에 두고 주도적으로 테스트 하며,</p>
             <p>피드백을 통해 인사이트를 도출 하겠습니다.</p>
